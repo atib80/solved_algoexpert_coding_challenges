56f18e74139c2b07a34ca870a67637f7a84edea8
Budai Attila <atib1980@gmail.com>
2021-08-01 22:30:29 +0200

    add valid_ip_address_generator class

--- medium/52_valid_ip_addresses.cpp
+++ medium/52_valid_ip_addresses.cpp
@@ -1,167 +1,195 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <limits>
 #include <string>
+#include <utility>
 #include <vector>
 
-using namespace std;
+class valid_ip_address_generator {
+  class iterator {
+    std::vector<std::string>::iterator pos_;
+    const std::vector<std::string>& ip_addresses_;
 
-vector<string> split(const string& source,
-                     const string& needle,
-                     const bool split_on_whole_needle = true,
-                     const bool ignore_empty_string = true,
-                     size_t const max_count = string::npos) {
-  const size_t source_len{source.length()};
-  if (0U == source_len)
-    return {};
+   public:
+    using iterator_category = std::forward_iterator_tag;
 
-  const size_t needle_len{split_on_whole_needle ? needle.length() : 1U};
+    explicit iterator(const std::vector<std::string>::iterator& pos,
+                      const std::vector<std::string>& ip_addresses)
+        : pos_{pos}, ip_addresses_{ip_addresses} {}
 
-  if (0U == needle_len) {
-    const size_t upper_limit{max_count < source_len ? max_count : source_len};
-    vector<string> parts(upper_limit);
-    for (size_t i{}; i < upper_limit; ++i)
-      parts[i] = string{1, source[i]};
-    return parts;
-  }
+    std::string& operator*() { return *pos_; }
+    const std::string& operator*() const { return *pos_; }
 
-  vector<string> parts{};
-  size_t number_of_parts{}, prev{};
-
-  while (true) {
-    const size_t current = split_on_whole_needle
-                               ? source.find(needle.c_str(), prev)
-                               : source.find_first_of(needle.c_str(), prev);
-
-    if (string::npos == current)
-      break;
-
-    if (string::npos != max_count && parts.size() == max_count)
-      break;
-
-    if (current - prev > 0 || !ignore_empty_string) {
-      if (!ignore_empty_string)
-        parts.emplace_back();
-      else
-        parts.emplace_back(cbegin(source) + prev, cbegin(source) + current);
-      number_of_parts++;
+    iterator& operator++() {
+      if (ip_addresses_.end() != pos_)
+        ++pos_;
+      return *this;
     }
 
-    prev = current + needle_len;
-
-    if (prev >= source_len)
-      break;
-  }
-
-  if (prev < source_len &&
-      (string::npos == max_count || parts.size() < max_count))
-    parts.emplace_back(cbegin(source) + prev, cend(source));
-
-  return parts;
-}
-
-size_t get_state_sum(const string& state) noexcept {
-  size_t sum{};
-  for (const char ch : state)
-    sum += ch - '0';
-  return sum;
-}
-
-bool get_next_state(
-    string& state,
-    const size_t desired_state_sum = numeric_limits<size_t>::max(),
-    const bool is_reset = false) {
-  static string state_{"3333"};
-  static bool is_repeated = false;
-
-  if (is_reset) {
-    state_ = "3333";
-    is_repeated = false;
-    return false;
-  }
-
-  do {
-    for (size_t i{state_.size()}; i > 0; --i) {
-      const size_t index{i - 1};
-      if ('3' == state_[index]) {
-        state_[index] = '1';
-      } else {
-        state_[index] += 1;
-        break;
-      }
+    iterator operator++(int) {
+      iterator temp{*this};
+      if (ip_addresses_.end() != pos_)
+        ++pos_;
+      return temp;
     }
 
-    if (state_ == "1111") {
-      is_repeated = !is_repeated;
-      if (!is_repeated) {
-        state = state_;
-        return false;
-      }
+    bool operator==(const iterator& other) const {
+      return this->pos_ == other.pos_;
+    }
+    bool operator!=(const iterator& other) const { return !(*this == other); }
+  };
+
+  class const_iterator {
+    std::vector<std::string>::const_iterator const_pos_;
+    const std::vector<std::string>& ip_addresses_;
+
+   public:
+    using iterator_category = std::forward_iterator_tag;
+
+    explicit const_iterator(
+        const std::vector<std::string>::const_iterator& const_pos,
+        const std::vector<std::string>& ip_addresses)
+        : const_pos_{const_pos}, ip_addresses_{ip_addresses} {}
+
+    const std::string& operator*() const { return *const_pos_; }
+
+    const_iterator& operator++() {
+      if (ip_addresses_.cend() != const_pos_)
+        ++const_pos_;
+      return *this;
+    }
+    const_iterator operator++(int) {
+      const_iterator temp{*this};
+      if (ip_addresses_.cend() != const_pos_)
+        ++const_pos_;
+      return temp;
     }
 
-  } while (desired_state_sum != numeric_limits<size_t>::max() &&
-           desired_state_sum != get_state_sum(state_));
-
-  state = state_;
-  return true;
-}
-
-bool check_if_ip_address_is_valid(const string& ip) {
-  const vector<string> ip_parts{split(ip, ".")};
-
-  if (ip_parts.size() != 4)
-    return false;
-
-  for (const auto& ip_part : ip_parts) {
-    if (!(ip_part.length() >= 1U && ip_part.length() <= 3U) ||
-        any_of(cbegin(ip_part), cend(ip_part),
-               [](const char ch) { return ch < '0' || ch > '9'; }) ||
-        ('0' == ip_part[0] && ip_part.length() != 1U) || (stoi(ip_part) > 255))
-      return false;
-  }
-
-  return true;
-}
-
-vector<string> validIPAddresses(const string& str) {
-  const size_t max_ip_address_length{str.length() + 3};
-
-  vector<string> valid_ip_addresses;
-  string state;
-
-  get_next_state(state, str.length(), true);
-
-  while (get_next_state(state, str.length())) {
-    size_t pos{};
-    string ip_address;
-    ip_address.reserve(max_ip_address_length);
-
-    for (const char s : state) {
-      if (0 != pos) {
-        ip_address.push_back('.');
-      }
-
-      for (size_t k{pos}, last = min(pos + (s - '0'), str.length()); k < last;
-           ++k) {
-        ip_address.push_back(str[k]);
-      }
-
-      pos += s - '0';
+    bool operator==(const const_iterator& other) const {
+      return this->const_pos_ == other.const_pos_;
     }
+    bool operator!=(const const_iterator& other) const {
+      return !(*this == other);
+    }
+  };
 
-    if (check_if_ip_address_is_valid(ip_address))
-      valid_ip_addresses.emplace_back(ip_address);
+ public:
+  valid_ip_address_generator(const std::string& ip_address_digits)
+      : ip_address_digits_{ip_address_digits}, state_{"3333"} {
+    generate_all_valid_ip_addresses();
   }
 
-  return valid_ip_addresses;
+  const std::vector<std::string>& get_all_valid_ip_addresses() const noexcept {
+    return valid_ip_addresses_;
+  }
+
+  iterator begin() {
+    return iterator{valid_ip_addresses_.begin(), valid_ip_addresses_};
+  }
+
+  iterator end() {
+    return iterator{valid_ip_addresses_.end(), valid_ip_addresses_};
+  }
+
+  const_iterator begin() const {
+    return const_iterator{valid_ip_addresses_.cbegin(), valid_ip_addresses_};
+  }
+
+  const_iterator end() const {
+    return const_iterator{valid_ip_addresses_.cend(), valid_ip_addresses_};
+  }
+
+  const_iterator cbegin() const {
+    return const_iterator{valid_ip_addresses_.cbegin(), valid_ip_addresses_};
+  }
+
+  const_iterator cend() const {
+    return const_iterator{valid_ip_addresses_.cend(), valid_ip_addresses_};
+  }
+
+ private:
+  void generate_all_valid_ip_addresses() const {
+    const size_t max_ip_address_length{ip_address_digits_.length() + 3};
+
+    while (true) {
+      for (size_t i{state_.size()}; i > 0; --i) {
+        const size_t index{i - 1};
+        if ('3' == state_[index]) {
+          state_[index] = '1';
+        } else {
+          state_[index] += 1;
+          break;
+        }
+      }
+
+      if (state_ == "1111") {
+        is_repeated_ = !is_repeated_;
+        if (!is_repeated_)
+          break;
+      }
+
+      if (ip_address_digits_.length() != get_state_sum(state_))
+        continue;
+
+      size_t pos{};
+      std::string ip_address;
+      ip_address.reserve(max_ip_address_length);
+
+      for (const char s : state_) {
+        std::string ip_part;
+
+        if (0 != pos) {
+          ip_address.push_back('.');
+        }
+
+        for (size_t k{pos},
+             last = std::min(pos + (s - '0'), ip_address_digits_.length());
+             k < last; ++k) {
+          ip_part.push_back(ip_address_digits_[k]);
+        }
+
+        if (!(ip_part.length() >= 1U && ip_part.length() <= 3U) ||
+            std::any_of(std::cbegin(ip_part), std::cend(ip_part),
+                        [](const char ch) { return ch < '0' || ch > '9'; }) ||
+            ('0' == ip_part[0] && ip_part.length() != 1U) ||
+            (stoi(ip_part) > 255))
+          break;
+
+        ip_address += ip_part;
+
+        pos += s - '0';
+      }
+
+      if (ip_address_digits_.length() == pos)
+        valid_ip_addresses_.emplace_back(std::move(ip_address));
+    }
+  }
+
+  size_t get_state_sum(const std::string& state) const noexcept {
+    size_t sum{};
+    for (const char ch : state)
+      sum += ch - '0';
+    return sum;
+  }
+
+  const std::string ip_address_digits_;
+  mutable std::string state_;
+  mutable std::vector<std::string> valid_ip_addresses_;
+  mutable bool is_repeated_ = false;
+};
+
+std::vector<std::string> validIPAddresses(const std::string& str) {
+  const valid_ip_address_generator gen{str};
+
+  return gen.get_all_valid_ip_addresses();
 }
 
 int main() {
-  const vector<string> valid_ip_addresses{validIPAddresses("1921680")};
+  const std::vector<std::string> valid_ip_addresses{
+      validIPAddresses("1921680")};
 
   for (const auto& valid_ip : valid_ip_addresses) {
-    cout << valid_ip << '\n';
+    std::cout << valid_ip << '\n';
   }
 
   return 0;
